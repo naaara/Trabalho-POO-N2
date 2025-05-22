a0016f60c4937eab7f3acd947cd979046c31aa59
João Manoel <joaomanoelsilvamorais@gmail.com>
2025-05-22 15:30:07 -0300

    Correção de alguns erros e criação de setters e getters da classe pessoa e da classe derivada secretaria.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,105 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
 
-namespace std;
+using namespace std;
 
-class pessoa{
-    private:
-        int codigo;
-        int cpf[12];
-        string nome;
-        string email;
-        string telefone;
-        string whatsapp;
+class pessoa {
+protected:
+    int codigo;
+    int cpf[12];
+    string nome;
+    string email;
+    string telefone;
+    string whatsapp;
+
+public:
+    pessoa() {
+        codigo = 0;
+        for (int i = 0; i < 12; i++) {
+            cpf[i] = 0;
+        }
+        nome = "";
+        email = "";
+        telefone = "";
+        whatsapp = "";
+    }
+    void setCodigo(int codin) {
+        codigo = codin;
+    }
+    int getCodigo() const {
+        return codigo;
+    }
+    void setCpf(int cpfin[12]) {
+        for (int i = 0; i < 12; i++) {
+            cpf[i] = cpfin[i];
+        }
+    }
+    int* getCpf() {
+        return cpf;
+    }
+    void setNome(string nomein) {
+        nome = nomein;
+    }
+
+    string getNome() const {
+        return nome;
+    }
+    void setEmail(string emailin) {
+        email = emailin;
+    }
+    string getEmail() const {
+        return email;
+    }
+    void setTelefone(string telefonein) {
+        telefone = telefonein;
+    }
+    string getTelefone() const {
+        return telefone;
+    }
+    void setWhatsapp(string whastappin) {
+        whatsapp = whastappin;
+    }
+    string getWhatsapp() const {
+        return whatsapp;
+    }
 };
 
-class secretaria : public pessoa{
-    private:
-        string login;
-        string senha;
-        small int cargo; // 0 - secretária, 1 - secretária-supervisora
+class secretaria : public pessoa {
+private:
+    string login;
+    string senha;
+    short cargo; // 0 - secretária, 1 - secretária-supervisora
 
-    void setLogin(string loginin){
+
+
+public:
+    void setLogin(string loginin) {
         login = loginin;
     }
-    void setSenha(string senhain){
+
+    void setSenha(string senhain) {
         senha = senhain;
     }
-};
-
-class medico : public pessoa{
 
 };
-
-class paciente : public pessoa{
-
+ana
+class medico : public pessoa {
+    // Implementação futura
 };
+
+class paciente : public pessoa {
+    // Implementação futura
+};
+
+int main() {
+    secretaria sec;
+    string nome_temp;
+
+    cout << "Digite o nome da secretária: ";
+    cin >> nome_temp;
+    sec.setNome(nome_temp);
+    cout << "Nome da secretária: " << sec.getNome() << endl;
+
+    return 0;
+}
